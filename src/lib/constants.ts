29450c0d8678afe36155653f24a23c7b60a5ee44
Ben Link <link.ben@gmail.com>
2024-03-04 10:35:53 -0500

    Updating to be about me

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,8 @@
 export const LINKS = {
-  github: "https://github.com/Ladvace",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
-  medium: "https://ladvace.medium.com/",
-  discord: "https://discordapp.com/users/163300027618295808",
-  dribble: "https://dribbble.com/Ladvace_Jace",
+  github: "https://github.com/LinkBenjamin",
+  linkedin: "https://www.linkedin.com/in/benjamindlink/",
+  blog: "https://dev.to/linkbenjamin",
+  email: "mailto:link.ben@gmail.com"
 };
 
 export const loaderAnimation = [
